33f2936c4a3d4bd6d9d018574c098ca47afc9489
Hugo Mozo <hmozo@outlook.com>
2021-01-28 21:10:41 +0100

    JdbcUserDetailsManager

--- src/main/java/com/manning/ssia/config/ProjectConfig.java
+++ src/main/java/com/manning/ssia/config/ProjectConfig.java
@@ -1,38 +1,27 @@
 package com.manning.ssia.config;
 
-import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetails;
+
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
-import com.manning.ssia.domain.SecurityUser;
-import com.manning.ssia.domain.User;
 
 @Configuration
 public class ProjectConfig{
 	
 	@Bean
-	public UserDetailsService userDetailsService() {
-		User user= new User(1L, "john", "123456", "read");
-		UserDetails securityUser= new SecurityUser(user);
-		List<UserDetails> users= List.of(securityUser);
-		
-		return new InMemoryUserDetailsManager(users);
+	public UserDetailsService userDetailsService(DataSource dataSource) {
+		return new JdbcUserDetailsManager(dataSource);
 	}
 	
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return NoOpPasswordEncoder.getInstance();
-	}
-	
+	}	
 }
--- src/main/java/com/manning/ssia/domain/User.java
+++ src/main/java/com/manning/ssia/domain/User.java
@@ -1,13 +1,20 @@
 package com.manning.ssia.domain;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 import lombok.Data;
 
 @Entity
+@Table(name = "users")
 @Data
 public class User {
 
+	@Id 
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private final Long id;
 	private final String username;
 	private final String password;
