aabccef5c31acd5dbb202c07136d43c919ae3c66
Hugo Mozo <hmozo@outlook.com>
2021-01-28 18:31:11 +0100

    AuthenticationManagerBuilder

--- src/main/java/com/manning/ssia/config/ProjectConfig.java
+++ src/main/java/com/manning/ssia/config/ProjectConfig.java
@@ -2,6 +2,7 @@ package com.manning.ssia.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -13,22 +14,24 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @Configuration
 public class ProjectConfig extends WebSecurityConfigurerAdapter{
 	
-	@Bean
-	public UserDetailsService userDetailsService() {
-	var userDetailsService =
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		var userDetailsService =
 			new InMemoryUserDetailsManager();
-			var user = User.withUsername("john")
+		var user = User.withUsername("john")
 			.password("123456")
 			.authorities("read")
 			.build();
-			userDetailsService.createUser(user);
-			return userDetailsService;
+		userDetailsService.createUser(user);
+		
+		auth.userDetailsService(userDetailsService)
+			.passwordEncoder(NoOpPasswordEncoder.getInstance());
 	}
 	
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-	return NoOpPasswordEncoder.getInstance();
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.httpBasic();
+		http.authorizeRequests().anyRequest().authenticated();
 	}
-	
 	
 }
