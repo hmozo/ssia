4ef7d233917f4c91032efaa8a7530fdc97e0649f
Hugo Mozo <hmozo@outlook.com>
2021-01-28 19:32:33 +0100

    AuthenticationProvider decoupled

--- src/main/java/com/manning/ssia/config/ProjectConfig.java
+++ src/main/java/com/manning/ssia/config/ProjectConfig.java
@@ -1,7 +1,9 @@
 package com.manning.ssia.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -14,18 +16,12 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @Configuration
 public class ProjectConfig extends WebSecurityConfigurerAdapter{
 	
+	@Autowired
+	private AuthenticationProvider authenticationProvider;
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		var userDetailsService =
-			new InMemoryUserDetailsManager();
-		var user = User.withUsername("john")
-			.password("123456")
-			.authorities("read")
-			.build();
-		userDetailsService.createUser(user);
-		
-		auth.userDetailsService(userDetailsService)
-			.passwordEncoder(NoOpPasswordEncoder.getInstance());
+		auth.authenticationProvider(this.authenticationProvider);
 	}
 	
 	@Override
